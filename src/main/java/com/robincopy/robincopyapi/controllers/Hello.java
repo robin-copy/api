f50fd9b6e6f2c3d767c8776a401b1feb1c361611
joacoiannuzzi <joaquin.iannuzzi@ing.austral.edu.ar>
2021-05-19 01:13:15 -0300

    trying something

---
+++ src/main/java/com/robincopy/robincopyapi/controllers/Hello.java
@@ -0,0 +1,15 @@
+package com.robincopy.robincopyapi.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class Hello {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello world";
+    }
+
+}
