6ea0f65dbb4ab9d0f3a19d63a642ffc56dec649f
joacoiannuzzi <joaquin.iannuzzi@ing.austral.edu.ar>
2021-05-19 00:25:48 -0300

    add files

---
+++ src/test/java/com/robincopy/robincopyapi/RobinCopyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robincopy.robincopyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobinCopyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
