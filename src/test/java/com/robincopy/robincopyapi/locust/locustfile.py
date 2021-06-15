e115efc5cdcf810af09cf3ca5ebf98078ba998cf
Braian Aniel Biale <braian.biale@ing.austral.edu.ar>
2021-06-15 20:20:13 -0300

    locust test progress

--- src/test/java/com/robincopy/robincopyapi/locust/locustfile.py
+++ src/test/java/com/robincopy/robincopyapi/locust/locustfile.py
@@ -1,22 +1,51 @@
 import uuid
-from locust import HttpUser, task, between
+from locust import HttpUser, task, TaskSet, between
 
 
-class RobinCopyTest(HttpUser):
-    wait_time = between(12, 13)
-
-    #@task
-    #def getShares(self):
-    #    self.client.get("/api/users/4028b8817a085189017a08518f910000/shares")
-
-    #@task
-    #def addNewShare(self):
-    #    self.client.post(url="/api/users/shares",
-    #                   json={"userId": "2c9ac0817a0d751d017a0d7521950000", "symbol": "TSLA", "quantity": "2"},
-    #                   headers={"Content-Type": "application/json"})
+class FinnHubApiGetShares(TaskSet):
+    user_id = "4028b8817a118a78017a118a7b7c0000"
 
     @task
-    def newUser(self):
+    def get_shares(self):
+        self.client.get("/api/users/{}/shares".format(self.user_id), name="get shares")
+
+
+class FinnHubApiBuyShare(TaskSet):
+    user_id = "4028b8817a118a78017a118a7b7c0000"
+
+    @task
+    def buy_share(self):
+        self.client.post(url="/api/users/shares",
+                         json={"userId": self.user_id, "symbol": "TSLA", "quantity": "2"},
+                         headers={"Content-Type": "application/json"}, name="buy share")
+
+
+class FinnHubApiGetUserShareInfoByStockSymbol(TaskSet):
+    user_id = "4028b8817a118a78017a118a7b7c0000"
+    stock_symbol = "TSLA"
+
+    @task
+    def get_share_info_by_stock_symbol(self):
+        self.client.get("/api/users/{}/shares/{}".format(self.user_id, self.stock_symbol), name="get shares")
+
+
+class CreateUser(TaskSet):
+    @task
+    def create_new_user(self):
         self.client.post(url="/api/users",
-                         json={"firstName": "random " + str(uuid.uuid4()), "lastName": "Papaa"},
-                         headers={"Content-Type": "application/json"})
+                         json={"firstName": "RandomName" + str(uuid.uuid4()), "lastName": "MyLastName"},
+                         headers={"Content-Type": "application/json"}, name="create new user")
+
+
+class UserTaskSet(TaskSet):
+    tasks = {CreateUser: 1, FinnHubApiGetShares: 5}
+
+
+class WebUser(HttpUser):
+    tasks = [UserTaskSet]
+    wait_time = between(2, 3)
+
+
+class FinnHubWebUser(HttpUser):
+    tasks = [FinnHubApiBuyShare]
+    wait_time = between(30, 32)
